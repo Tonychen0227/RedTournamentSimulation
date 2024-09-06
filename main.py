4a1b10fe766ad7c6d084b5db2f95621d06cfd2bc
hwangbro <nickhwangbo@gmail.com>
2024-09-05 22:45:15 -0700

    Easier player generation

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from models.player import Player
-
-
-### ELO RANGES
-# High tier - 2000 - 1800
-# Mid tier - 1800 - 1700
-# Bottom tier - 1700 - 1500
 from models.tournament import Tournament
 
-players = [
-    Player("Top High", 2000),
-    Player("Mid High", 1900),
-    Player("Bot High / Top Mid", 1800),
-    Player("Mid Mid", 1750),
-    Player("Bot Mid / Top Bottom", 1700),
-    Player("Mid Bottom", 1600),
-    Player("Bottom Bottom", 1500)
-]
 
+def generate_players(mean_elo: int, range_sizes: list[int]) -> list[Player]:
+    players = []
+    players.append(Player("Top High", mean_elo + range_sizes[1]/2 + range_sizes[0]))
+    players.append(Player("Mid High", mean_elo + range_sizes[1]/2 + range_sizes[0]/2))
+    players.append(Player("Bot High / Top Mid", mean_elo + range_sizes[1]/2))
+    players.append(Player("Mid Mid", mean_elo))
+    players.append(Player("Bot Mid / Top Bottom", mean_elo - range_sizes[1]/2))
+    players.append(Player("Mid Bottom", mean_elo - range_sizes[1]/2 - range_sizes[2]/2))
+    players.append(Player("Bottom Bottom", mean_elo - range_sizes[1]/2 - range_sizes[2]))
 
-def run_sim_and_report(player_1, player_2, player_3):
+    return players
+
+def elo_ranges(mean_elo: int, range_sizes: list[int]) -> list[str]:
+    ret = []
+    ret.append(f"High Tier: {mean_elo + range_sizes[1]/2} - {mean_elo + range_sizes[1]/2 + range_sizes[0]}")
+    ret.append(f"Mid Tier: {mean_elo - range_sizes[1]/2} - {mean_elo + range_sizes[1]/2}")
+    ret.append(f"Bottom Tier: {mean_elo - range_sizes[1]/2 - range_sizes[2]} - {mean_elo - range_sizes[1]/2}")
+
+    return ret
+
+def run_sim_and_report(player_1: Player, player_2: Player, player_3: Player):
     contestants = [player_1, player_2, player_3]
 
     rates = []
@@ -32,11 +36,19 @@ def run_sim_and_report(player_1, player_2, player_3):
     print("MATCH PREDICTION")
     for x in range(0, len(contestants)):
         print(f"{contestants[x].name}, win chance {round(rates[x] * 100, 2)}%")
-    print("")
+    print()
 
 
-run_sim_and_report(players[0], players[3], players[6])
-run_sim_and_report(players[0], players[1], players[2])
-run_sim_and_report(players[2], players[3], players[4])
+if __name__ == "__main__":
+    mean_elo = 1400
+    range_sizes = [300, 400, 800]
+    players = generate_players(mean_elo, range_sizes)
+    print("ELO Ranges:")
+    for elo_range in elo_ranges(mean_elo, range_sizes):
+        print(elo_range)
 
-tournament = Tournament(49)
+    print()
+    run_sim_and_report(players[0], players[3], players[6])
+    run_sim_and_report(players[0], players[1], players[2])
+    run_sim_and_report(players[2], players[3], players[4])
+    tournament = Tournament(49)
